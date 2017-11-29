0e572e7b0257445cd67fd132abc3893308daabe4
ellbrady <bradyelliot@yahoo.com>
2017-11-29 09:44:52 -0800

    initial commit

---
+++ LinearSearch.java
@@ -0,0 +1,10 @@
+package algorithms;
+
+public class LinearSearch {
+	
+	public static void Search(int[] numbers, int target) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+package algorithms;
+
+public class Main {
+
+	public static void main(String[] args){
+		int[] numbers = {10,25, 433, 26, 73, 132, 5};
+		
+		// Call a function that searches for a specific number.
+		algorithms.LinearSearch.Search(numbers, 73);
+	}
+
+}
